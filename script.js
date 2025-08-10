d5ed618a3be788199af3e3e56de728c669db85fb
zspxx <ismayilzadazhala@gmail.com>
2025-08-10 17:29:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -29,8 +29,10 @@ $(document).ready(function() {
 
   function createConfetti() {
     const container = document.getElementById('confetti-container');
-    const colors = ['#cdb4db', '#b497bd', '#9a72ac', '#7b5e7a', '#a593e0'];
-    for (let i = 0; i < 100; i++) {
+    // const colors = ['#cdb4db', '#b497bd', '#9a72ac', '#7b5e7a', '#a593e0']; - lilac
+    const colors = ['#ffe6f0', '#ffd6e0', '#ffc0cb', '#ffb6c1', '#ff9bb9']; //soft pink
+    const count = 150;
+    for (let i = 0; i < count; i++) {
       const confetti = document.createElement('div');
       confetti.classList.add('confetti');
       confetti.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
